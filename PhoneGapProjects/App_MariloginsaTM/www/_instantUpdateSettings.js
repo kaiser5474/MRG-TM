fb12b1ef08e89be4e82b11b6abbed9da8f48ca97
Jarvis <jarvismiranda@gmail.com>
2020-02-07 14:13:58 -0500

    prueba

---
+++ PhoneGapProjects/App_MariloginsaTM/www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "5f473969b09b44cda8babb000749290b",
+	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": false,
+	"message": "",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": "Prueba"
+};
+}
