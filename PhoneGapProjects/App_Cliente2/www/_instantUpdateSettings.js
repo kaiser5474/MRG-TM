8f29309fd11af8de96482cdf78b9304fd92a0a4b
jarvismiranda728096@gmail.com <jarvismiranda728096@gmail.com>
2021-03-07 23:41:25 -0500

    11111

---
+++ PhoneGapProjects/App_Cliente2/www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "aad77b4b4d704c8ab5a8283d11e713b6",
+	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": true,
+	"message": "Version Actualizada",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": "Bienvenido a MariloginsaTM"
+};
+}
